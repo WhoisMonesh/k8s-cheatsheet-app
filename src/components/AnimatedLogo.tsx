932cd3a068561fdc30238d2c794a96fbe44cc5f4
Monesh Ram <moneshram7@gmail.com>
2026-01-29 17:17:55 +0530

    Update app icon to modern Kubernetes logo

--- src/components/AnimatedLogo.tsx
+++ src/components/AnimatedLogo.tsx
@@ -17,29 +17,31 @@ export function AnimatedLogo() {
           </linearGradient>
         </defs>
         
-        {/* Hexagon Shape */}
+        {/* Kubernetes Heptagon */}
         <path
-          d="M50 5L93.3 30V80L50 105L6.7 80V30L50 5Z"
+          d="M50 5 L85.2 21.9 L93.9 60 L69.5 90.5 L30.5 90.5 L6.1 60 L14.8 21.9 Z"
           fill="url(#logoGradient)"
           className="opacity-90"
         />
         
-        {/* Inner Detail - Helm Wheel Stylized */}
-        <path
-          d="M50 35V65M35 50H65M40 40L60 60M60 40L40 60"
-          stroke="white"
-          strokeWidth="6"
-          strokeLinecap="round"
-          className="opacity-90"
-        />
+        {/* Inner Wheel - 7 Spokes */}
+        <g transform="translate(50, 50)" fill="white" className="opacity-90">
+          <circle r="8" />
+          {[0, 1, 2, 3, 4, 5, 6].map((i) => (
+            <g key={i} transform={`rotate(${i * (360 / 7)})`}>
+              <rect x="-3" y="-28" width="6" height="16" rx="2" />
+              <rect x="-6" y="-34" width="12" height="6" rx="2" />
+            </g>
+          ))}
+        </g>
         
         {/* Orbiting Dot */}
-        <circle r="4" fill="white" className="animate-spin-slow origin-center opacity-80">
-           <animateMotion 
-             dur="4s" 
-             repeatCount="indefinite"
-             path="M50 15 C 70 15, 85 30, 85 50 C 85 70, 70 85, 50 85 C 30 85, 15 70, 15 50 C 15 30, 30 15, 50 15 Z"
-           />
+        <circle r="3" fill="white" className="animate-spin-slow origin-center opacity-80">
+          <animateMotion 
+            dur="6s" 
+            repeatCount="indefinite"
+            path="M50 10 C 80 10, 90 30, 90 50 C 90 80, 70 95, 50 95 C 30 95, 10 80, 10 50 C 10 30, 20 10, 50 10 Z"
+          />
         </circle>
       </svg>
     </div>
